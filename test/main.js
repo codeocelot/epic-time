cb363acce8f542dd841a8225e54a4c22b392d0c2
codeocelot <joeygracey@gmail.com>
2016-01-21 13:55:00 -0800

    wrote second test, refactored code

--- test/main.js
+++ test/main.js
@@ -3,12 +3,20 @@ var epoch = require('../index');
 var moment = require('moment');
 var assert = require('assert');
 
-console.log(epoch().toString());
 describe('get random times',()=>{
   it('should not give me today\'s date',()=>{
     var now = moment(), rand = epoch();
     var isRandom = ['y','M','d','h','m','s','ms'].some(u=>now.get(u) != rand.get(u));
-		console.log(now.toString(),rand.toString());
     assert(isRandom);
+  })
+  it('should return a different date each time',()=>{
+    var times = [];
+    for(var i = 0;i < 10; i++){
+      times.push(epoch());
+    }
+    while(times.length > 0){
+      var t = times.pop();
+      assert(!times.some(el=>el == t));
+    }
   })
 })
