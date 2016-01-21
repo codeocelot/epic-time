3f406ea3cba90db45faadc4c61815fc7c2cc0603
codeocelot <joeygracey@gmail.com>
2016-01-21 15:35:13 -0800

    replaced == with ===

--- js/epic-time.js
+++ js/epic-time.js
@@ -1,9 +1,17 @@
-module.exports = function(){
+module.exports = function(num){
+  if(!num) num = 1;
   var moment = require('moment');
-  var epochs = ['y','M','d','h','m','s','ms'], dur = {};
-  epochs.forEach(e=>{
-    dur[e] = Math.floor(Math.random() * 20) + 1 ;
-  });
-  if(Math.floor(Math.random()*2) % 2) return moment().add(dur);
-  else return moment().subtract(dur);
+  var epochs = ['y','M','d','h','m','s','ms'], dur = {}, times = [];
+  for(var i = 0; i < num; i++){
+    epochs.forEach(e=>{
+      dur[e] = Math.floor(Math.random() * 20) + 1 ;
+    });
+    if(Math.floor(Math.random()*2) % 2) times.push(moment().add(dur));
+    else times.push(moment().subtract(dur));
+  }
+  if(num === 1){
+    return times[0]
+  } else {
+    return times;
+  }
 }
--- test/main.js
+++ test/main.js
@@ -3,10 +3,10 @@ var epoch = require('../index');
 var moment = require('moment');
 var assert = require('assert');
 
-describe('get random times',()=>{
+describe('get a single random date',()=>{
   it('should not give me today\'s date',()=>{
     var now = moment(), rand = epoch();
-    var isRandom = ['y','M','d','h','m','s','ms'].some(u=>now.get(u) != rand.get(u));
+    var isRandom = ['y','M','d','h','m','s','ms'].some(u=>now.get(u) !== rand.get(u));
     assert(isRandom);
   })
   it('should return a different date each time',()=>{
@@ -16,7 +16,20 @@ describe('get random times',()=>{
     }
     while(times.length > 0){
       var t = times.pop();
-      assert(!times.some(el=>el == t));
+      assert(!times.some(el=>el === t));
+    }
+  });
+})
+
+describe('get multiple dates',()=>{
+  var times = epoch(10);
+  it('should return an array',()=>{
+    assert(times.length === 10);
+  });
+  it('should return non-identical dates',()=>{
+    while(times.length > 0){
+      var t = times.pop();
+      assert(!times.some(el=> el === t));
     }
   })
 })
